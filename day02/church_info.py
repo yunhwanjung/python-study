5e368808c7e5d4b3e6edf4d4e605d7e6e1c95a0e
jung yun hwan <silentagape@gmail.com>
2026-01-15 07:16:33 +0900

    Day 2: 변수 학습 완료

---
+++ day02/church_info.py
@@ -0,0 +1,11 @@
+# 교회 정보
+church_name = "수서동행교회"
+pastor_name = "정윤환 목사"
+address = "서울시 강남구 수서동"
+member_count = 12
+
+#출력하기
+print("교회명:", church_name)
+print("목사:", pastor_name)
+print("주소:", address)
+print("성도:", member_count)
---
+++ day02/variables.py
@@ -0,0 +1,13 @@
+# 변수 만들기 - 설교 제목 저장하기 
+sermon_title = "사랑의 계명"
+
+#성경 본문 
+bible_verse = "요한복음 18장 17절"
+
+#날짜
+sermon_date = "2025년 1월 18일"
+
+# 모두 출력하기
+print(sermon_title)
+print(bible_verse)
+print(sermon_date)
